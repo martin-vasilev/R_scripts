f8144424a8d2cc6a04bad98e4ffcc233872e9025
Martin R. Vasilev <martin.r.vasilev@gmail.com>
2019-02-04 20:00:56 +0000

    Update visAngle.py

--- visAngle.py
+++ visAngle.py
@@ -6,19 +6,14 @@ Created on Wed Oct 31 13:25:02 2018
 """
 # from: https://osdoc.cogsci.nl/3.1/visualangle/
 
-def visAngle(height, width, distance, size):
+def visAngle(h= 70, d= 80, r= 1920, size=12):
     
-    from math import atan2, degrees
-    # height: Monitor height in cm
-    # distance: Distance between monitor and participant in cm
-    # width: Vertical resolution of the monitor
-    # size: The stimulus size in pixels
-    
-    # Calculate the number of degrees that correspond to a single pixel. This will
-    # generally be a very small value, something like 0.03.
-    deg_per_px = degrees(atan2(.5*height, distance)) / (.5*width)
-
-    # Calculate the size of the stimulus in degrees
-    size_in_deg = size * deg_per_px
-    
-    return size_in_deg
+   from math import atan2, degrees
+   # Calculate the number of degrees that correspond to a single pixel. This will
+   # generally be a very small value, something like 0.03.
+   deg_per_px = degrees(atan2(.5*h, d)) / (.5*r)
+   print('%s degrees correspond to a single pixel' % deg_per_px)
+   # Calculate the size of the stimulus in degrees
+   size_in_deg = size * deg_per_px
+   print('The size of the stimulus is %s pixels and %s visual degrees' \
+        % (size, size_in_deg))
