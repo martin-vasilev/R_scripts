4792e1a57ab5ad7bfd8cf34de9d2c2d8d838ccaf
Martin R. Vasilev <martin.r.vasilev@gmail.com>
2021-02-14 08:53:05 +0000

    Add files via upload

---
+++ color_blind_palletes.R
@@ -0,0 +1,17 @@
+
+##### color-blind palletes:
+
+## Author: David Nichols
+#  https://davidmathlogic.com/colorblind/#%23332288-%23117733-%2344AA99-%2388CCEE-%23DDCC77-%23CC6677-%23AA4499-%23882255
+
+
+cb_pallete1 <- c("#332288", "#117733", "#44AA99", "#88CCEE", "#DDCC77", "#CC6677",
+                 "#AA4499", "#882255")
+
+cb_pallete2 <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2",
+                 "#D55E00", "#CC79A7")
+
+
+cb_pari1<- c("#FFC20A", "#0C7BDC")
+cb_pari2<- c("#E1BE6A", "#40B0A6")
+cb_pair3<- c("#E66100", "#5D3A9B")
